8d79a65b37f039d6fc75bba90f73a2d68bdaaf0f
joao-ryan <joaoryanmaranhaodecastro@gmail.com>
2026-03-03 09:58:13 -0300

    questao tres item quatro adicionado

---
+++ questao03-java/ex04/exerc04ex3d.java
@@ -0,0 +1,14 @@
+public class EXERC04EX3D {
+    public static void main(String[] args) {
+
+        int numero = 1;
+
+        while (numero < 30) {
+            if (numero % 4 == 0) {
+                System.out.println(numero);
+            }
+            numero++;
+        }
+
+    }
+}
