ad7c40e00f6bccb9c1528d758487f3c274819db6
joao-ryan <joaoryanmaranhaodecastro@gmail.com>
2026-03-03 09:57:01 -0300

    Questão tres item um adicionado

---
+++ questao03-java/ex01/exerc04ex3a.java
@@ -0,0 +1,13 @@
+public class EXERC04EX3A {
+    public static void main(String[] args) {
+
+        int numero = 15;
+
+        while (numero <= 200) {
+            int quadrado = numero * numero;
+            System.out.println(numero + "² = " + quadrado);
+            numero++;
+        }
+
+    }
+}
