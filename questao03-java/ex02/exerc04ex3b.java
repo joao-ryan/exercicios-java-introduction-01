faca5557998e57fcfa1c14372b4b16a98fb36253
joao-ryan <joaoryanmaranhaodecastro@gmail.com>
2026-03-03 09:57:22 -0300

    questao tres item dois adicionado

---
+++ questao03-java/ex02/exerc04ex3b.java
@@ -0,0 +1,15 @@
+public class EXERC04EX3B {
+    public static void main(String[] args) {
+
+        int numero = 1;
+        int soma = 0;
+
+        while (numero <= 100) {
+            soma += numero;
+            numero++;
+        }
+
+        System.out.println("Soma de 1 até 100 = " + soma);
+
+    }
+}
