beb739d55d2eaf4d9742e003b094036b2a95967c
joao-ryan <joaoryanmaranhaodecastro@gmail.com>
2026-03-03 09:57:45 -0300

    questao tres item tres adicionado

---
+++ questao03-java/ex03/exerc04ex3c.java
@@ -0,0 +1,14 @@
+public class EXERC04EX3C {
+    public static void main(String[] args) {
+
+        int numero = 1;
+
+        while (numero <= 20) {
+            if (numero % 2 != 0) {
+                System.out.println(numero);
+            }
+            numero++;
+        }
+
+    }
+}
